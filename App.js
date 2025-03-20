49f14d04e176b040ac49f4e4256c8bc7a22761ed
assioutymining <assioutymining@gmail.com>
2025-03-20 23:05:09 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GoldWallet from "./components/GoldWallet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>محفظة الذهب الفاخرة</h1>
+      <GoldWallet />
+    </div>
+  );
+}
+
+export default App;
